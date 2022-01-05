9ab059770c1699206579677f98cdda1e25b49324
Steven Montoya <steve5mv5@gmail.com>
2022-01-05 23:44:29 +0100

    implement Header Component

---
+++ client/components/Header/HeaderComponent.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {HeaderWrapper, HeaderTitle } from "./styled"
+
+const HeaderComponent = ({}) => {
+  return (
+    <HeaderWrapper>
+      <img src="https://bob.io/wp-content/uploads/2019/05/Logo-Line-Red.png"/>
+      <HeaderTitle>Baggage service</HeaderTitle>
+    </HeaderWrapper>
+    
+
+  )
+}
+
+export default HeaderComponent
---
+++ client/components/Header/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+ 
+ export const HeaderWrapper = styled.div`
+    box-shadow: 0 5px #ccc;
+    display: flex;
+    align-items: center;
+ `
+ export const HeaderTitle = styled.h1`
+   color: rgb(223, 39, 50);
+   margin-left: 2%;
+   font-family: "Arial"
+ `
